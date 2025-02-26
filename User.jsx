160b6960fd7c1f667bca54b4a417c3c9b0025b4d
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-26 13:37:14 -0500

    Create User.jsx

---
+++ User.jsx
@@ -0,0 +1,16 @@
+class User extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      message : "Welcome to React world!"
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    )
+  }
+}
